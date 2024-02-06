e10a87c684e91f1878f85ac7543b3f25e2753be7
André Maree <ammaree@users.noreply.github.com>
2024-02-06 23:03:36 +0200

    Update copyright year

--- pca9555.h
+++ pca9555.h
@@ -1,5 +1,5 @@
 /*
- * pca9555.h - Copyright (c) 2014-23 Andre M. Maree/KSS Technologies (Pty) Ltd.
+ * pca9555.h - Copyright (c) 2014-24 Andre M. Maree/KSS Technologies (Pty) Ltd.
  */
 
 #pragma once
