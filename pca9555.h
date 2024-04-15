749407db10b081fbbea03e98348f2e130613bb52
André Maree <ammaree@users.noreply.github.com>
2024-04-15 22:47:57 +0200

    Update pca9555.h

--- pca9555.h
+++ pca9555.h
@@ -1,6 +1,4 @@
-/*
- * pca9555.h - Copyright (c) 2014-24 Andre M. Maree/KSS Technologies (Pty) Ltd.
- */
+// pca9555.h
 
 #pragma once
 
@@ -10,16 +8,8 @@ extern "C" {
 
 
 // ######################################## Enumerations ###########################################
-
-
 // ######################################### Structures ############################################
-
-struct i2c_di_t;
-struct report_t;
-
 // ####################################### Public variables ########################################
-
-
 // ####################################### Global functions ########################################
 
 void pca9555DIG_IN_Config(u8_t pin);
@@ -33,12 +23,14 @@ int pca9555DIG_OUT_WriteAll(void);
 int	pca9555DIG_OUT_GetState(u8_t pin);
 void pca9555DIG_OUT_Toggle(u8_t pin);
 
+struct i2c_di_t;
 int	pca9555Diagnostics(struct i2c_di_t * psI2C);
 int	pca9555Identify(struct i2c_di_t * psI2C);
 int	pca9555Config(struct i2c_di_t * psI2C);
 int	pca9555Check(void);
 
-int pca9555Report(report_t * psR);
+struct report_t;
+int pca9555Report(struct report_t * psR);
 
 #ifdef __cplusplus
 }
