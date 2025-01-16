8dd44b5b2ded3d3c897c3d96ddb8d7e13356a64e
André Maree <ammaree@users.noreply.github.com>
2025-01-16 17:02:08 +0200

    Improve API documentation

--- pca9555.h
+++ pca9555.h
@@ -19,9 +19,25 @@ u8_t pca9555DIG_IN_GetState(u8_t pin);
 void pca9555DIG_IN_Invert(u8_t pin);
 
 void pca9555DIG_OUT_Config(u8_t pin);
-void pca9555DIG_OUT_SetStateLazy(u8_t pin, u8_t State);
-void pca9555DIG_OUT_SetState(u8_t pin, u8_t State);
-int pca9555DIG_OUT_WriteAll(void);
+
+/**
+ * @brief	Check if buffer is dirty, if so write to device.
+ * @return	1/true if status written else 0/false
+ */
+bool pca9555DIG_OUT_WriteAll(void);
+
+/**
+ * @brief	Update buffered pin status without writing new status to device
+ * @return	1/true if buffer is dirty/changed else 0/false
+ */
+bool pca9555DIG_OUT_SetStateLazy(u8_t pin, u8_t State);
+
+/**
+ * @brief	Update buffered pin status and [if changed] write status to device
+ * @return	1/true if status written else 0/false
+ */
+bool pca9555DIG_OUT_SetState(u8_t pin, u8_t State);
+
 int	pca9555DIG_OUT_GetState(u8_t pin);
 void pca9555DIG_OUT_Toggle(u8_t pin);
 
