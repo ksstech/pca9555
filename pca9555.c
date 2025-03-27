f4dd97829f375c34d7b54c034437a8f002205cfc
André Maree <ammaree@users.noreply.github.com>
2025-03-27 09:11:38 +0200

    Simplify APIs, create common device read and write signature

--- pca9555.c
+++ pca9555.c
@@ -12,6 +12,7 @@
 // ########################################## MACROS ###############################################
 
 #define	debugFLAG					0xF000
+#define	debugFLUSH					(debugFLAG & 0x0001)
 #define	debugTIMING					(debugFLAG_GLOBAL & debugFLAG & 0x1000)
 #define	debugTRACK					(debugFLAG_GLOBAL & debugFLAG & 0x2000)
 #define	debugPARAM					(debugFLAG_GLOBAL & debugFLAG & 0x4000)
@@ -47,34 +48,27 @@ static int pca9555ReadRegister(u8_t Reg) {
 	return halI2C_Queue(sPCA9555.psI2C, i2cWDR_FB, &cChr, sizeof(cChr), (u8_t *) &sPCA9555.Regs[Reg], sizeof(u16_t), (i2cq_p1_t) NULL, (i2cq_p2_t) NULL);
 }
 
-static int pca9555WriteRegister(u8_t Reg, u16_t Val) {
-	sPCA9555.Regs[Reg] = Val;
 /**
  * @brief
  * @param[in]
  * @param[in]
  * @return
  */
+static int pca9555WriteRegister(u8_t Reg) {
 	u8_t cBuf[3];
 	cBuf[0] = Reg << 1;									// force to u16_t boundary 0 / 2 / 4 / 6
-	cBuf[1] = Val >> 8;
-	cBuf[2] = Val & 0xFF;
-	int iRV = halI2C_Queue(sPCA9555.psI2C, i2cW_FB, cBuf, sizeof(cBuf), (u8_t *) NULL, 0, (i2cq_p1_t) NULL, (i2cq_p2_t) NULL);
-	if (Reg == pca9555_OUT && iRV == erSUCCESS)
-		sPCA9555.fDirty = 0;							// show as clean, just written
-	return iRV;
+	cBuf[1] = sPCA9555.Regs[Reg] >> 8;
+	cBuf[2] = sPCA9555.Regs[Reg] & 0xFF;
+	return halI2C_Queue(sPCA9555.psI2C, i2cW_FB, cBuf, sizeof(cBuf), (u8_t *) NULL, 0, (i2cq_p1_t) NULL, (i2cq_p2_t) NULL);
 }
 
-void pca9555Reset(void) {
-	pca9555WriteRegister(pca9555_CFG, pca9555Cfg);
-	pca9555WriteRegister(pca9555_POL, pca9555Pol);
-	pca9555WriteRegister(pca9555_OUT, pca9555Out);
-}
 
 
 int pca9555Flush(void) {
 	if (sPCA9555.fDirty) {
-		pca9555WriteRegister(pca9555_OUT, sPCA9555.Regs[pca9555_OUT]);
+		IF_CPT(debugFLUSH, "Flush x%04X" strNL, sPCA9555.Regs[pca9555_OUT]);
+		pca9555WriteRegister(pca9555_OUT);
+		sPCA9555.fDirty = 0;							// show as clean, just written
 		return 1;
 	}
 	return 0;
@@ -91,32 +85,32 @@ int pca9555Function(pca9555func_e Func, u8_t Pin, bool NewState) {
 		IF_myASSERT(debugTRACK, (sPCA9555.Regs[pca9555_CFG] & Mask) == 0);
 		bool CurState = (sPCA9555.Regs[pca9555_OUT] & Mask) ? 1 : 0;
 		if (Func <= stateTGL) {							// stateTGL[_LAZY]
-			NewState = sPCA9555.Regs[pca9555_OUT] & Mask ? 0 : 1;
-			Func += (stateSET_LAZY - stateTGL_LAZY);	// adjust stateTGL[_LAZY] -> stateSET[_LAZY]
+			NewState = !CurState;;						// calculate NewState
+			Func += (stateSET_LAZY - stateTGL_LAZY);	// stateTGL[_LAZY] -> stateSET[_LAZY]
 		}
 		if (NewState != CurState) {
 			if (NewState)	sPCA9555.Regs[pca9555_OUT] |= Mask;
 			else			sPCA9555.Regs[pca9555_OUT] &= ~Mask;
 			sPCA9555.fDirty = 1;						// bit just changed, show as dirty
 		}
-		return (Func <= stateSET_LAZY) ? sPCA9555.fDirty : pca9555Flush();
+		return (Func < stateSET) ? sPCA9555.fDirty : pca9555Flush();
 
 	} else if (Func == stateGET) {						// Can be INPut or OUTput....
 		if (sPCA9555.Regs[pca9555_CFG] & Mask) {		// configured as INput ?
 			int iRV = pca9555ReadRegister(pca9555_IN);	// read live status
 			return (iRV == erSUCCESS) ? ((sPCA9555.Regs[pca9555_IN] & Mask) ? 1 : 0) : xSyslogError(__FUNCTION__, iRV);
-		} else {										// configured as OUTput
-			return (sPCA9555.Regs[pca9555_OUT] & Mask) ? 1 : 0;
 		}
+		return (sPCA9555.Regs[pca9555_OUT] & Mask) ? 1 : 0; // configured as OUTput
 
 	} else if (Func == cfgINV) {						// MUST be INput
 		IF_myASSERT(debugTRACK, (sPCA9555.Regs[pca9555_CFG] & Mask) == 1);
-		pca9555WriteRegister(pca9555_POL, sPCA9555.Regs[pca9555_POL] ^ Mask);
+		sPCA9555.Regs[pca9555_POL] ^= Mask;
+		pca9555WriteRegister(pca9555_POL);
 
 	} else if (Func == cfgDIR) {						// Direction INput vs OUTput
 		if (NewState)	sPCA9555.Regs[pca9555_CFG] |= Mask;
 		else			sPCA9555.Regs[pca9555_CFG] &= ~Mask;
-		pca9555WriteRegister(pca9555_CFG, sPCA9555.Regs[pca9555_CFG]);
+		pca9555WriteRegister(pca9555_CFG);
 	}
 	return 0;
 }
@@ -160,78 +154,92 @@ int	pca9555Identify(i2c_di_t * psI2C) {
 	psI2C->TObus = 25;
 	psI2C->Test	= 1;
 	// Step 1 - ensure all set to defaults
-	int iRV = pca9555WriteRegister(pca9555_POL, 0);		// default non inverted/normal
+	sPCA9555.Regs[pca9555_POL] = 0x0000;				// non inverted/normal
+	int iRV = pca9555WriteRegister(pca9555_POL);
 	if (iRV < erSUCCESS)
-		return iRV;
-	iRV = pca9555WriteRegister(pca9555_CFG, 0xFFFF);	// default all Inputs
+		goto exit;
+	sPCA9555.Regs[pca9555_CFG] = 0xFFFF;				// all inputs
+	iRV = pca9555WriteRegister(pca9555_CFG);
 	if (iRV < erSUCCESS)
-		return iRV;
+		goto exit;
 	// Step 2 - read all registers
 	for (int r = pca9555_IN; r < pca9555_NUM; ++r) {
 		 iRV = pca9555ReadRegister(r);
 		 if (iRV < erSUCCESS)
-		 	return iRV;
+		 	goto exit;
 	}
 	// Step 3 - Check initial default values
 	if (sPCA9555.Regs[pca9555_POL] != 0 || sPCA9555.Regs[pca9555_CFG] != 0xFFFF)
 		return erINV_WHOAMI;
-	u16_t OrigOUT = sPCA9555.Regs[pca9555_OUT];			// passed phase 1, now step 4
-	pca9555WriteRegister(pca9555_CFG, 0);				// all OUTputs
+	u16_t OrigOUT = sPCA9555.Regs[pca9555_OUT];			// passed steps 1~3, now step 4
+	sPCA9555.Regs[pca9555_CFG] = 0x0000;				// all OUTputs
+	pca9555WriteRegister(pca9555_CFG);
 	pca9555ReadRegister(pca9555_OUT);
 	if (sPCA9555.Regs[pca9555_OUT] != OrigOUT)
 		return erINV_WHOAMI;
 	psI2C->IDok = 1;
 	psI2C->Test	= 0;
+exit:
 	return iRV;
 }
 
 int	pca9555Config(i2c_di_t * psI2C) {
+	int iRV = erINV_STATE;
 	if (psI2C->IDok == 0)
-		return erINV_STATE;
+		goto exit;
 	psI2C->CFGok = 0;
 	halEventUpdateDevice(devMASK_PCA9555, 0);
-	int iRV = pca9555WriteRegister(pca9555_CFG, pca9555Cfg);	// IN vs OUT
+	sPCA9555.Regs[pca9555_CFG] = pca9555Cfg;
+	iRV = pca9555WriteRegister(pca9555_CFG);			// set required direction, IN vs OUT
 	if (iRV < erSUCCESS)
-		return iRV;
-	iRV = pca9555WriteRegister(pca9555_POL, pca9555Pol);	// Non Invert
+		goto exit;
+	sPCA9555.Regs[pca9555_POL] = pca9555Pol;
+	iRV = pca9555WriteRegister(pca9555_POL);			// set required invertion status
 	if (iRV < erSUCCESS)
-		return iRV;
-	iRV = pca9555WriteRegister(pca9555_OUT, pca9555Out);	// All OUTputs
+		goto exit;
+	sPCA9555.Regs[pca9555_OUT] = pca9555Out;
+	iRV = pca9555WriteRegister(pca9555_OUT);			// set required output status (optional)
 	if (iRV < erSUCCESS)
-		return iRV;
+		goto exit;
 	psI2C->CFGok = 1;
 	sPCA9555.fDirty = 0;
 	halEventUpdateDevice(devMASK_PCA9555, 1);
 	// once off init....
 	if (psI2C->CFGerr == 0)
 		IF_SYSTIMER_INIT(debugTIMING, stPCA9555, stMICROS, "PCA9555", 200, 3200);
+exit:
 	return iRV;
 }
 
 int	pca9555Diagnostics(i2c_di_t * psI2C) {
 	// configure as outputs and display
 	wprintfx(NULL, "Default (all Outputs )status" strNL);
-	pca9555WriteRegister(pca9555_CFG, 0x0000);
+	sPCA9555.Regs[pca9555_CFG] = 0x0000;
+	pca9555WriteRegister(pca9555_CFG);
 	vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 
 	// set all OFF and display
 	wprintfx(NULL, "All outputs (OFF) status" strNL);
-	pca9555WriteRegister(pca9555_OUT, 0x0000);
+	sPCA9555.Regs[pca9555_OUT] = 0x0000;
+	pca9555WriteRegister(pca9555_OUT);
 	vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 
 	// set all ON and display
 	wprintfx(NULL, "All outputs (ON) status" strNL);
-	pca9555WriteRegister(pca9555_OUT, 0xFFFF);
+	sPCA9555.Regs[pca9555_OUT] = 0xFFFF;
+	pca9555WriteRegister(pca9555_OUT);
 	vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 
 	// set all OFF and display
 	wprintfx(NULL, "All outputs (OFF) status" strNL);
-	pca9555WriteRegister(pca9555_OUT, 0x0000);
+	sPCA9555.Regs[pca9555_OUT] = 0x0000;
+	pca9555WriteRegister(pca9555_OUT);
 	vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 
 	// set all back to inputs and display
 	wprintfx(NULL, "All Inputs (again) status" strNL);
-	pca9555WriteRegister(pca9555_CFG, 0xFFFF);
+	sPCA9555.Regs[pca9555_CFG] = 0xFFFF;
+	pca9555WriteRegister(pca9555_CFG);
 	vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 
 	// Change INput to OUTput(0) and turn ON(1)
@@ -248,7 +256,6 @@ int	pca9555Diagnostics(i2c_di_t * psI2C) {
 		pca9555Toggle(Pin);
 		vTaskDelay(pdMS_TO_TICKS(pca9555TEST_INTERVAL));
 	}
-	pca9555Reset();
 	wprintfx(NULL, "Diagnostics completed. All LEDs = OFF !!!" strNL);
 	return erSUCCESS;
 }
